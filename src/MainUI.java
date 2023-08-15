4b60927a290e50bd3edfd7ababbfd621f065e290
Rishik Patel <135653264+rishikpatel123@users.noreply.github.com>
2023-08-15 12:16:02 +0530

    Update MainUI.java

--- src/MainUI.java
+++ src/MainUI.java
@@ -4,7 +4,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.*;
 
-//https://github.com/rishikpatel123/upskill_campus
+//https://github.com/rishikpatel123/Expense_Tracker
 public class MainUI {
     private static void addexp()
     {
