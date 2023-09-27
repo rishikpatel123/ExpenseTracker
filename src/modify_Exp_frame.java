2e36990c0467de33e9f5e979344f544a171a769a
Rishik Patel <135653264+rishikpatel123@users.noreply.github.com>
2023-09-27 23:28:11 +0530

    Update modify_Exp_frame.java

--- src/modify_Exp_frame.java
+++ src/modify_Exp_frame.java
@@ -32,8 +32,8 @@ import java.util.Calendar;
                 try {
                     // Establish the database connection (Replace with your actual database credentials)
                     String url = "jdbc:mysql://db4free.net:3306/expenseeee";
-                    String username = "rishik";
-                    String password = "a512be21";
+                    String username = "";
+                    String password = "";
                     Connection conn = DriverManager.getConnection(url, username, password);
 
                     // Prepare the SQL query for fetching entries for the selected date
