c0dd4daa6dc990196460660df8ff4ffd93adb4a9
Rishik Patel <135653264+rishikpatel123@users.noreply.github.com>
2023-12-15 11:28:32 +0530

    Update AddExpenseFrame.java

--- src/AddExpenseFrame.java
+++ src/AddExpenseFrame.java
@@ -74,9 +74,9 @@ public class AddExpenseFrame {
 
             try {
                 // Establish the database connection (Replace with your actual database credentials)
-                String url = "jdbc:mysql://db4free.net:3306/expenseeee";
-                String username = "";
-                String password = "";
+                String url = "";//your db url
+                String username = "";//your db username
+                String password = "";//your db password
                 Connection conn = DriverManager.getConnection(url, username, password);
 
                 // Prepare the SQL query for inserting the expense data
